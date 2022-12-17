2da9eaf425161328c231624de6c05b8a6f1c56a0
handro.dev <handro.dev@gmail.com>
2022-12-17 09:58:37 +0100

    Implemented Day 6 part 1

--- Day06.py
+++ Day06.py
@@ -5,7 +5,23 @@ from Utils import *
 
 
 def part1(input):
-    return len(input)
+    # Input is only one line in this case
+    input = input[0]
+    marker_buffer = []
+
+    for idx, char in enumerate(input):
+        if len(marker_buffer) < 4:
+            if char in marker_buffer:
+                # Character is repeated
+                # Remove first character until the repeated one
+                found_at = marker_buffer.index(char)
+                del marker_buffer[:found_at+1]
+            marker_buffer.append(char)
+        else:
+            # Found four different characters
+            return idx
+
+    return -1
 
 def part2(input):
     return len(input)
@@ -22,6 +38,8 @@ def main(input):
 
 if __name__ == "__main__":
     filename = os.path.splitext(sys.argv[0])[0]
-    test_part(f"data/{filename}_test.txt", part1, 0)
-    test_part(f"data/{filename}_test.txt", part2, 0)
+    
+    test_part(f"data/{filename}_test.txt", part1, 10)
+    # test_part(f"data/{filename}_test.txt", part2, 0)
+
     main(f"data/{filename}.txt")
