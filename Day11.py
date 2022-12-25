691cfb7de54da74aaf4ea5e144d69ce9cebf54f0
handro.dev <handro.dev@gmail.com>
2022-12-25 22:28:48 +0100

    Finish implementing solution for day 11

--- Day11.py
+++ Day11.py
@@ -8,49 +8,24 @@ from Utils import *
 
 
 class Monkey:
-    def __init__(self, items, operator, operand, div_test, div_true, div_false, divide_by_3=True):
+    def __init__(self, items, operation, div_test, div_true, div_false):
         self.items = items
-        self.operand = operand
-        self.operator = operator
+        self.operation = lambda old: eval(operation)
         self.div_test = div_test
         self.div_true = div_true
         self.div_false = div_false
         self.inspected_items = 0
-        self.divide_by_3 = divide_by_3
-    
-    def inspect(self, divide_by_3=True):
-        result = []
-        self.inspected_items += len(self.items)
 
-        while len(self.items) > 0:
-            obj = self.items.pop(0)
-
-            if self.operand == 'old':
-                operand_val = obj
-            else:
-                operand_val = int(self.operand)
-
-            if self.operator == '*':
-                obj *= operand_val
-            elif self.operator == '+':
-                obj += operand_val
-            
-            if self.divide_by_3:
-                obj //= 3
-            result.append((obj, self.test(obj)))
-        
-        return result
-
-    def test(self, obj):
-        if obj % self.div_test == 0:
+    def test(self, value):
+        if value % self.div_test == 0:
             return self.div_true
-        
+
         return self.div_false
 
-def solution(input, rounds=20, divide_by_3=True):
-    monkeys = []
+def solution(input, rounds=20, divide_by_three=True):
+    monkeys = {}
     items_regex = re.compile("(\d+)(?:,\s*)?")
-    op_regex = re.compile("\s*Operation: new = old ([\*\+]) (\d+|old)")
+    op_regex = re.compile("\s*Operation: new = (.*)")
 
     # Parse input, get monkeys and create list of monkey objects
     # With corresponding operations
@@ -62,29 +37,35 @@ def solution(input, rounds=20, divide_by_3=True):
         monkey_input = input[i*6:(i+1)*6]
         
         items = list(map(int, items_regex.findall(monkey_input[1])))
-        operator, operand = op_regex.match(monkey_input[2]).groups()
+        operation = op_regex.match(monkey_input[2]).group(1)
 
         div_test = int(monkey_input[3].split()[-1])
         div_true = int(monkey_input[4].split()[-1])
         div_false = int(monkey_input[5].split()[-1])
 
-        monkeys.append(Monkey(items, operator, operand, div_test, div_true, div_false, divide_by_3))
+        monkeys[i] = Monkey(items, operation, div_test, div_true, div_false)
+
+    # Compute product of all monkey divisors
+    divisor = reduce(mul, [m.div_test for m in monkeys.values()], 1)
 
     # Start simulation for as many rounds as required
-    for c in range(rounds):
-        for m in range(num_monkeys):
-            result = monkeys[m].inspect()
-        
-            for item in result:
-                obj, to_monkey = item[0], item[1]
-                monkeys[to_monkey].items.append(obj)
-        
-        # print(f"After round {c+1}:")
-        # for idx, m in enumerate(monkeys):
-        #     print(f"Monkey {idx}: {m.items}")
-        # print()
+    for r in range(rounds):
+        for m in monkeys.values():
+            curr_items = m.items
+            m.inspected_items += len(curr_items)
 
-    return reduce(mul, list(sorted([m.inspected_items for m in monkeys], reverse=True))[0:2])
+            for item in curr_items:
+                new_item = m.operation(item)
+                if divide_by_three:
+                    new_item //= 3
+                new_item %= divisor
+
+                target_monkey = m.test(new_item)
+                monkeys[target_monkey].items.append(new_item)
+
+            m.items = []
+
+    return reduce(mul, list(sorted([m.inspected_items for m in monkeys.values()], reverse=True))[0:2], 1)
 
 def main(input):
     lines = read_input(input)
@@ -94,10 +75,10 @@ def main(input):
         return
     
     print(solution(lines, 20, True))
-    print(solution(lines, 1000, False))
+    print(solution(lines, 10000, False))
 
 if __name__ == "__main__":
     filename = os.path.splitext(sys.argv[0])[0]
-    # test_part(f"data/{filename}_test.txt", lambda x: solution(x, 20, True), 10605)
+    test_part(f"data/{filename}_test.txt", lambda x: solution(x, 20, True), 10605)
     test_part(f"data/{filename}_test.txt", lambda x: solution(x, 10000, False), 2713310158)
-    # main(f"data/{filename}.txt")
+    main(f"data/{filename}.txt")
