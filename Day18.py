90f708d8e7a50a47d5a24660837785c075d06e66
handro.dev <handro.dev@gmail.com>
2023-01-07 19:59:53 +0100

    Implement solution for day 18 part 2

--- Day18.py
+++ Day18.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from queue import Queue
 
 from Utils import *
 
@@ -30,13 +31,79 @@ def compute_surface(cubes: set[tuple[int, int, int]]) -> int:
     return surface
 
 
+def has_path_to_ext(cube: tuple[int, int, int],
+                    cubes: set[tuple[int, int, int]],
+                    boundaries: tuple[int, int, int, int, int, int],
+                    trapped: set[tuple[int, int, int]],
+                    not_trapped: set[tuple[int, int, int]]) -> bool:
+    """
+    Returns True if the specified cube has a path to the exterior (not trapped) 
+    and False otherwise (trapped)
+    """
+    q = Queue()
+    q.put(cube)
+
+    min_x, max_x, min_y, max_y, min_z, max_z = boundaries
+
+    visited = set()
+    while not q.empty():
+        c = q.get()
+        x, y, z = c
+
+        if c in not_trapped:
+            # Already know that this cube has a path to the environment
+            return True
+
+        if c in trapped or c in cubes:
+            # Already know that this cube is trapped
+            # or path is blocked by another cube
+            continue
+
+        if any([x > max_x, x < min_x, y > max_y, y < min_y, z > max_z, z < min_z]):
+            return True
+
+        for adj in adjacent(c):
+            if adj not in visited:
+                visited.add(adj)
+                q.put(adj)
+
+    # Could not find a path, cube is trapped
+    return False
+
+
+def compute_ext_surface(cubes: set[tuple[int, int, int]]) -> int:
+    min_x, max_x = float('+inf'), float('-inf')
+    min_y, max_y = float('+inf'), float('-inf')
+    min_z, max_z = float('+inf'), float('-inf')
+
+    for x, y, z in cubes:
+        min_x, max_x = min(x, min_x), max(x, max_x)
+        min_y, max_y = min(y, min_y), max(y, max_y)
+        min_z, max_z = min(z, min_z), max(z, max_z)
+
+    boundaries = (min_x, max_x, min_y, max_y, min_z, max_z)
+    trapped = set([])
+    not_trapped = set([])
+    ext_surface = 0
+    for c in cubes:
+        for adj in adjacent(c):
+            if has_path_to_ext(adj, cubes, boundaries, trapped, not_trapped):
+                not_trapped.add(adj)
+                ext_surface += 1
+            else:
+                trapped.add(adj)
+
+    return ext_surface
+
+
 def part1(input):
     cubes = set(parse_input(input))
     return compute_surface(cubes)
 
 
 def part2(input):
-    return len(input)
+    cubes = set(parse_input(input))
+    return compute_ext_surface(cubes)
 
 
 def main(input):
@@ -47,11 +114,11 @@ def main(input):
         return
 
     print(part1(lines))
-    # print(part2(lines))
+    print(part2(lines))
 
 
 if __name__ == "__main__":
     filename = os.path.splitext(sys.argv[0])[0]
     test_part(f"data/{filename}_test.txt", part1, 64)
-    # test_part(f"data/{filename}_test.txt", part2, 58)
+    test_part(f"data/{filename}_test.txt", part2, 58)
     main(f"data/{filename}.txt")
